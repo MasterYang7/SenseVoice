de426c35ece057f7a2ed5f07edacd44ac6d797aa
yangshifu <yangshifu@wodcloud.com>
2024-11-06 19:01:08 +0800

    fx

--- ysf.py
+++ ysf.py
@@ -3,7 +3,7 @@ import torch_npu
 from funasr.utils.postprocess_utils import rich_transcription_postprocess
 
 model_dir = "iic/SenseVoiceSmall"
-torch_npu.init()
+
 
 model = AutoModel(
     model=model_dir,
