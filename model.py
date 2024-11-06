120f70113c6a93b593a0d24a47b31e24daa983ee
yangshifu <yangshifu@wodcloud.com>
2024-11-06 18:46:05 +0800

    fix

--- model.py
+++ model.py
@@ -44,7 +44,7 @@ class SinusoidalPositionEncoder(torch.nn.Module):
 
     def forward(self, x):
         batch_size, timesteps, input_dim = x.size()
-        positions = torch.arange(1, timesteps + 1, device=x.device).to('npu')[None, :]
+        positions = torch.arange(1, timesteps + 1, device=x.device)[None, :]
         position_encoding = self.encode(positions, input_dim, x.dtype).to(x.device)
 
         return x + position_encoding
