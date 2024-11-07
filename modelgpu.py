a76c7e0913b9aac24513e8a62430243631b7daf0
yangshifu <yangshifu@wodcloud.com>
2024-11-07 17:54:23 +0800

    add

--- modelgpu.py
+++ modelgpu.py
@@ -26,7 +26,6 @@ class SinusoidalPositionEncoder(torch.nn.Module):
     ):
         batch_size = positions.size(0)
         positions = positions.type(dtype)
-        positions= positions.to('npu')
         device = positions.device
         print('驱动版本',positions)
         log_timescale_increment = torch.log(torch.tensor([10000], dtype=dtype, device=device)) / (
@@ -38,14 +37,14 @@ class SinusoidalPositionEncoder(torch.nn.Module):
         inv_timescales = torch.reshape(inv_timescales, [batch_size, -1])
         scaled_time = torch.reshape(positions, [1, -1, 1]) * torch.reshape(
             inv_timescales, [1, 1, -1]
-        ).to('npu')
+        )
         encoding = torch.cat([torch.sin(scaled_time), torch.cos(scaled_time)], dim=2)
         return encoding.type(dtype)
 
     def forward(self, x):
         batch_size, timesteps, input_dim = x.size()
         positions = torch.arange(1, timesteps + 1, device=x.device)[None, :]
-        position_encoding = self.encode(positions.to('npu'), input_dim, x.dtype).to(x.device)
+        position_encoding = self.encode(positions, input_dim, x.dtype).to(x.device)
 
         return x + position_encoding
 
