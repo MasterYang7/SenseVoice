a11dd3a0c17dc7557897016aa2b8ea6465082441
yangshifu <yangshifu@wodcloud.com>
2024-11-08 16:22:49 +0800

    add

--- demo_onnx.py
+++ demo_onnx.py
@@ -7,13 +7,18 @@ from pathlib import Path
 from funasr_onnx import SenseVoiceSmall
 from funasr_onnx.utils.postprocess_utils import rich_transcription_postprocess
 
-
+start_time = time.perf_counter()
 model_dir = "iic/SenseVoiceSmall"
 
 model = SenseVoiceSmall(model_dir, batch_size=10, quantize=True)
 
 # inference
-wav_or_scp = ["{}/.cache/modelscope/hub/{}/example/en.mp3".format(Path.home(), model_dir)]
-
+#wav_or_scp = ["{}/.cache/modelscope/hub/{}/example/en.mp3".format(Path.home(), model_dir)]
+wav_or_scp = ["./ysf.wav"]
 res = model(wav_or_scp, language="auto", use_itn=True)
 print([rich_transcription_postprocess(i) for i in res])
+end_time = time.perf_counter()
+
+# 计算运行时间
+execution_time = end_time - start_time
+print(f"代码运行时间: {execution_time} 秒")
